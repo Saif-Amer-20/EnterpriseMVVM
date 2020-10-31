1d189dca7db615e640e294dc5a043d53cf9bfa15
Saif Amer <saifamer18@hotmail.com>
2020-10-31 20:36:57 +0300

    fix issues and new services and apis 😄

--- EnterpriseMVVM.UI/MainWindow.xaml.cs
+++ EnterpriseMVVM.UI/MainWindow.xaml.cs
@@ -1,16 +1,29 @@
 ﻿using EnterpriseMVVM.UI.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 namespace EnterpriseMVVM.UI
 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
     public partial class MainWindow : Window
     {
         private MainWindowViewModel _mainWindowViewModel;
-
         public MainWindow(MainWindowViewModel mainWindowViewModel)
         {
             InitializeComponent();
-
             _mainWindowViewModel = mainWindowViewModel;
             DataContext = _mainWindowViewModel;
         }
